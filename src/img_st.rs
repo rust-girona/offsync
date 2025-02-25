6a62d41125778abe197edb72e4525f00f4186785
orambla64 <orambla@gmail.com>
2025-02-25 21:43:42 +0100

    wip: img_st load dir and tests

--- src/img_st.rs
+++ src/img_st.rs
@@ -1,18 +1,53 @@
-use std::collections::BTreeMap;
 use std::hash::{DefaultHasher, Hash, Hasher};
-use std::fs;
+use std::collections::BTreeMap;
+use std::process;
+use std::io;
+use std::time::SystemTime;
+use std::fs::{self, DirEntry};
+use std::path::{Path, PathBuf};
 
-// ImageStat struct 
+
 #[derive(Debug)]
 #[derive(Hash)]
-struct ImgSt {
-    path: String,
-    hash: u64,
-    st_size: u64,
-    st_mtime: u64
+pub struct ImgSt {
+    pub path: PathBuf,
+    pub st_size: u64,
+    pub st_mtime: SystemTime
 }
 
-/* 
+pub fn new_image_stat(path: &PathBuf) -> std::io::Result<ImgSt> {
+//    let path_buf = PathBuf::from(path);
+    let stat = fs::metadata(path)?;
+    let img_st:ImgSt = ImgSt {
+            path: PathBuf::from(path),
+            st_size: stat.len(),
+            st_mtime: stat.modified()?
+    };
+    Ok(img_st)
+}
+
+impl ImgSt {
+  pub fn new(path: &PathBuf) -> ImgSt {
+    let res= new_image_stat(path);
+    let img_st = match res {
+        Ok(img ) =>  img,
+        Err(err) =>  {
+            println!("Error {} reading image file {}", err, path.display());
+//         Err(err) =>  panic!("Error reading image file {}", path);
+            process::exit(1);
+        }
+    };
+    img_st
+  }
+}
+
+fn calculate_hash<T: Hash>(t: &T) -> u64 {
+    let mut s = DefaultHasher::new();
+    t.hash(&mut s);
+    s.finish()
+}
+
+/*
 impl Hash for ImgSt {
     fn hash<H: Hasher>(&self, state: &mut H) {
         self.id.hash(state);
@@ -21,58 +56,110 @@ impl Hash for ImgSt {
 }
 */
 
-
-// assert_eq!(calculate_hash(&person1), calculate_hash(&person2));
-
-fn calculate_hash<T: Hash>(t: &T) -> u64 {
-    let mut s = DefaultHasher::new();
-    t.hash(&mut s);
-    s.finish()
+struct ImgMap {
+    img_map: BTreeMap<u64, ImgSt> // BTreeMap for storing names (strings) and scores (u32)
 }
 
-
-fn getImgSt(path:String) -> ImgSt {
-    let stat = fs::metadata(path);
-    let imgSt = ImgSt { 
-        path: path, hash: 0, // calculate_hash(path), 
-        st_size: stat.expect("File not found").len(),
-        st_mtime: stat.expect("File not found").modified()
-    };
-    imgSt
-}
-
-
-pub(crate) fn test_data() {
-  let mut imgMap: BTreeMap<u64, ImgSt> = BTreeMap::new(); // BTreeMap for storing names (strings) and scores (u32)
-
-  let imgSt1 : ImgSt = ImgSt {
-    path: String::from("/home/orambla/Pictures/Dni1.jpg"),
-    hash: 1,
-    st_size: 1024,
-    st_mtime: 1234567890
-  };
-  let imgSt2  : ImgSt = ImgSt {
-    path: String::from("/home/orambla/Pictures/Dni2.jpg"),
-    hash: 2,
-    st_size: 1024,
-    st_mtime: 1234567890
-  };
-
-  let hash1 = calculate_hash(&imgSt1);
-  let hash2 = calculate_hash(&imgSt2);
-  imgMap.insert(hash1, imgSt1);
-  imgMap.insert(hash2, imgSt2);
-
-  // Access a value by key
-  let result: Option<&ImgSt> = imgMap.get(&hash1);
-  match result {
-    Some(img) => println!("Img1: {:?}", img),
-    None => println!("No image found")
+impl ImgMap {
+  pub fn new() -> ImgMap {
+    ImgMap {
+        img_map : BTreeMap::new() // BTreeMap for storing names (strings) and scores (u32)
+    }
   }
-//   println!("Img1: {:?}", img1);  
 
-  // Iterate over key-value pairs in sorted order
-  for (hash, img) in &imgMap {
-    println!("{}: {}", hash, img.path);
+  pub fn insert(&mut self, img_st: ImgSt)-> u64 {
+    let hash: u64 = calculate_hash(&img_st);
+    // let hash: u64 = img_st.hash(); 
+    self.img_map.insert(hash, img_st);
+    hash
+  }
+
+  pub fn get(&self, hash: &u64) -> Option<&ImgSt>{
+    self.img_map.get(&hash)
+  }
+
+  pub fn print(&self) {
+    // Iterate over key-value pairs in sorted order
+    for (hash, img) in &self.img_map {
+      print_img_st(hash, img);
+    }
+  }
+}  
+
+/*
+static mut img_map: BTreeMap<u64, ImgSt> = BTreeMap::new(); // BTreeMap for storing names (strings) and scores (u32)
+
+pub fn insert_img_st(img_st: ImgSt) -> u64 {
+    let hash: u64 = calculate_hash(&img_st);
+    // let hash: u64 = img_st.hash(); 
+    unsafe {
+        img_map.insert(hash, img_st);
+    }
+    hash
+}
+
+fn read_img_dir(path:&str) {
+    let paths = fs::read_dir(dir).unwrap();
+    for path in paths {
+        let path = path.unwrap().path();
+        let img_st = ImgSt::new(&path);
+        insert_img_st(img_st);
+    }
+}       
+*/
+
+fn visit_dirs(dir: PathBuf, img_map: &mut ImgMap, cb: &dyn Fn(&DirEntry, &mut ImgMap)) -> io::Result<()> {
+    if dir.is_dir() {
+        for entry in fs::read_dir(dir)? {
+            let entry = entry?;
+            let path = entry.path();
+            if path.is_dir() {
+                visit_dirs(path, img_map, cb)?;
+            } else {
+                cb(&entry, img_map);
+            }
+        }
+    }
+    Ok(())
+}
+
+pub fn load_dir(img_map: &ImgMap, dir: PathBuf) -> io::Result<()> {
+    visit_dirs(dir, img_map, &|entry, img_map| {
+        let path = entry.path();
+        let img_st = ImgSt::new(&path);
+        img_map.insert(img_st);
+    })
+}
+
+pub fn print_img_st(hash:&u64, img: &ImgSt) {
+      println!("img_st:{}:{}:{}:{}", *hash, img.path.display(),img.st_size, img.st_mtime.elapsed().unwrap().as_secs());
+}
+
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  #[test]
+  pub fn test_files() {
+    let mut img_map : ImgMap = ImgMap::new();
+    let img_st1 : ImgSt = ImgSt::new(&PathBuf::from("/home/orambla/Pictures/Dni1.jpg"));
+    let img_st2  : ImgSt = ImgSt::new(&PathBuf::from("/home/orambla/Pictures/Dni2.jpg"));
+    let hash1 = img_map.insert(img_st1);
+    let _hash2 = img_map.insert(img_st2);  
+
+    // Access a value by key
+    let result: Option<&ImgSt> = img_map.get(&hash1);
+    match result {
+      Some(img) => println!("Img1: {:?}", img),
+      None => println!("No image found")
+    }
+    img_map.print();
+  }
+
+  #[test]
+  pub fn test_dir() {
+    let mut img_map : ImgMap = ImgMap::new();
+    load_dir(&mut img_map, PathBuf::from("/home/orambla/Pictures")).unwrap();
+    img_map.print();
   }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::process;
+use std::path::{Path, PathBuf};
+use std::io;
+
+mod img_st;
+use img_st::*;
+
 fn main() {
     println!("Hello, world!");
 }
